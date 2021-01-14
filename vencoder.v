21e8dfad2af6feb7c29f5da20e6210d9ef7c5e2a
Erik Fox <erfox@auto.ece.pdx.edu>
2021-01-13 20:02:27 -0800

    compiled

--- vencoder.v
+++ vencoder.v
@@ -2,29 +2,25 @@ module vencoder(clock, reset, in, out);
 input logic clock, in, reset;
 output logic out;
 
-reg [0:2]x; 
-reg slowclock;
+reg [0:2]x=3'b000; 
+reg slowclock=0;
 //create slow clock
 always @ (posedge clock)
 	slowclock<=~slowclock;
 
 //combinational logic with blocking assignments into the output
 //po
-always @ (posedge slowclock)//is this working with the updated data from reg shift?
-begin
-	out=x[0]^x[1];
-end
+
 //p1
-always @ (negedge slowclock)     
-begin
+always@(negedge slowclock)
 	out=x[0]^x[1]^x[2];
-end
 //shift register of inputs
 always@(posedge slowclock)
 begin
 	x[0]<= in;
 	x[1]<=x[0];
 	x[2]<=x[1];
+	out<=in^x[0]^x[1];
 end
 
 //async reset
