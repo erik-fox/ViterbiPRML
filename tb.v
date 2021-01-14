2fb66206c289715effd154c1c002c47e7edd34a7
Erik Fox <erfox@auto.ece.pdx.edu>
2021-01-13 21:46:22 -0800

    compiles; at first glance works; needs verification and async reset

--- tb.v
+++ tb.v
@@ -1,12 +1,13 @@
 module top();
-reg  reset=0, in=1, Clock;
-wire out=0;
+reg  reset=0, in=1, Clock, slow=0;
+wire out;
+
 parameter SIMTIME = 1000;
 parameter TRUE   = 1'b1;
 parameter FALSE  = 1'b0;
 parameter CLOCK_CYCLE  = 20;
 parameter CLOCK_WIDTH  = CLOCK_CYCLE/2;
-reg slow=0;
+
 vencoder v0(Clock, reset, in, out);
 //
 // Create free running clock
@@ -21,6 +22,7 @@ end
 always @(posedge Clock)
 begin
 	slow=~slow;
+	$display("tb Output - %b",out);
 end
 always @(negedge slow)
 begin
--- vencoder.v
+++ vencoder.v
@@ -7,15 +7,11 @@ reg slowclock=0;
 //create slow clock
 always @ (posedge clock)
 	slowclock<=~slowclock;
-initial
-begin
-	$monitor("register = %b", x);
-end
 
 always@(negedge slowclock)
 begin
 	out=x[0]^x[1];
-	$display("negedge out = %b", out);
+//	$display("negedge out = %b", out);
 end
 //shift register of inputs
 always@(posedge slowclock)
@@ -25,9 +21,10 @@ begin
 	x[2]<=x[1];
 	$display("in %b x[0] %b x[1]" , in,x[0],x[1]); 
 	out =  in^x[0]^x[1];
-	$display(" posedge out - %b",out);
+//	$display(" posedge out - %b",out);
 end
-
+//always @(slowclock)
+	//$display("output - %b", out);
 //async reset
 always @(reset)
 	{slowclock,x}=4'b000;    
