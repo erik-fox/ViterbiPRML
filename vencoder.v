67fbe9ae29312d93a2a7f5bc4849add6eaf8dbfc
Erik Fox <erfox@auto.ece.pdx.edu>
2021-01-13 16:44:37 -0800

    Just follow the schematic

--- vencoder.v
+++ vencoder.v
@@ -1,5 +1,7 @@
 module vencoder(clock, reset, in, out);
 input logic clock, in, reset;
 output logic out;
-  
+
+// follow the design with procedural statements.  just design it like the
+// schematic.
 endmodule
