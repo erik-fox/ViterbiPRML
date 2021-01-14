86ab4a704323382f4b606670881e31a3879a5d55
Erik Fox <erfox@auto.ece.pdx.edu>
2021-01-13 18:13:39 -0800

    formatting

--- vencoder.v
+++ vencoder.v
@@ -6,22 +6,24 @@ reg [0:2]x;
 reg slowclock;
 //create slow clock
 always @ (posedge clock)
-  slowclock<=~slowclock;
+	slowclock<=~slowclock;
 
 //combinational logic with blocking assignments into the output
- always @ (posedge slowclock)//is this working with the new data?
- //po
- always @ (negedge slowclock)     
- //p1
+always @ (posedge slowclock)//is this working with the new data?
+//po
+always @ (negedge slowclock)     
+//p1
+
 //shift register of inputs
 always@(posedge slowclock)
 begin
-  x[0]<= in;
-  x[1]<=x[0];
-  x[2]<=x[1];
+	x[0]<= in;
+	x[1]<=x[0];
+	x[2]<=x[1];
 end
+
 //async reset
 always @(reset)
-  {slowclock,x}=4'b000;    
+	{slowclock,x}=4'b000;    
 
 endmodule
