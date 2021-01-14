595c5602cd34437ae7c2be4f667fae298ddadc91
erik-fox <erikfox589@gmail.com>
2021-01-13 17:59:32 -0800

    Update vencoder.v

--- vencoder.v
+++ vencoder.v
@@ -2,10 +2,23 @@ module vencoder(clock, reset, in, out);
 input logic clock, in, reset;
 output logic out;
 
-// follow the design with procedural statements.  just design it like the
-// schematic.
-  
+reg [0:2]x; 
+reg slowclock;
+//create slow clock
+always @ (posedge clock)
+  slowclock=~slowclock;
+
+//combinational logic with blocking assignments
+ 
+//shift register of inputs
 always@(posedge slowclock)
 begin
+  x[0]<= in;
+  x[1]<=x[0];
+  x[2]<=x[1];
 end
+//async reset
+always @(reset)
+  x=3'b000;    
+
 endmodule
