029b4d62e2101e8e208ca7041ab28ec82e81df80
Erik Fox <erfox@auto.ece.pdx.edu>
2021-01-17 18:21:43 -0800

    rough draft

--- vdecoder.v
+++ vdecoder.v
@@ -2,17 +2,55 @@ module vdecoder(clock, reset, in, out, error);
 input logic clock, in, reset;
 output logic out, error;
 reg slowclock=0;
+reg [0:1] state;
+reg [0:1] x=2'b00;
 always @ (posedge clock)
 	slowclock<=~slowclock;
+
+always @( posedge slowclock)
+begin
+	case(state)
+		2'b00:
+			if(x==2'b01 || x==2'b10)
+				error=1;
+			else
+				error=0;
+		2'b10:
+			if(x==2'b01 || x==2'b10)
+				error=1;
+			else
+				error=0;
+		2'b11:
+			if(x==2'b00 || x==2'b11)
+				error=1;
+			else
+				error=0;
+		2'b01:
+			if(x==2'b00 || x==2'b11)
+				error=1;
+			else
+				error=0;
+
+	endcase
+	out= (((~state[0])&&x[1])||(state[0]&& (~x[1])))&&(~error);
+end
 
 always @(negedge reset)
 begin
 	//set state back to zero
+	state=2'b00;
+	slowclock=0;
 end
-
+// change state
 always@(posedge slowclock)
 begin
-always@(negedge slowclock)
+	state[0]<=x[0];
+	state[1]<=x[1];	
+end
+//take inputs
+always@(posedge clock)
 begin
+	x[0]<= in;
+	x[1]<= x[0];
 end
 endmodule
